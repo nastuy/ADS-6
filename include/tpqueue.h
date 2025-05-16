953e82fc7373b76328fa7be17f03dbd99b52a3e7
nastuy <nastuysandalova@gmail.com>
2025-05-16 07:25:44 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,75 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
 struct SYM {
   char ch;
   int prior;
+  SYM* next;
+};
+
+template<typename T>
+class TPQueue {
+ private:
+  SYM* head;
+  SYM* tail;
+  int count;
+  int capacity;
+
+ public:
+  TPQueue() : head(nullptr), tail(nullptr), count(0), capacity(-1) {}
+  TPQueue(int size) : head(nullptr), tail(nullptr), count(0), capacity(size) {
+    if (size <= 0) {
+      throw std::string("Queue size must be positive");
+    }
+  }
+  bool isFull() const {
+    return capacity != -1 && count == capacity;
+  }
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+  void push(const SYM& item) {
+    if (isFull()) {
+      throw std::string("Queue is full");
+    }
+    SYM* tepm = new SYM{ item.ch, item.prior, nullptr };
+    if (isEmpty()) {
+      head = tail = tepm;
+    } else if (item.prior > head->prior) {
+      tepm->next = head;
+      head = tepm;
+    } else {
+      SYM* current = head;
+      while (current->next && current->next->prior >= item.prior) {
+        current = current->next;
+      }
+      tepm->next = current->next;
+      current->next = tepm;
+      if (tepm->next == nullptr) {
+        tail = tepm;
+      }
+    }
+    count++;
+  }
+  SYM pop() {
+    if (isEmpty()) {
+      throw std::string("Queue is empty");
+    }
+    SYM* temp = head;
+    SYM result = *temp;
+    head = head->next;
+    if (head == nullptr) {
+      tail = nullptr;
+    }
+    delete temp;
+    count--;
+    return result;
+  }
+  ~TPQueue() {
+    while (!isEmpty()) {
+      pop();
+    }
+  }
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
